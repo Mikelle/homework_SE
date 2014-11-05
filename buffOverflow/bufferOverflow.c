62bba3cd4e0ab458e983f674495dfa5f999c9ba8
Mikhail Wall <mikhwall@gmail.com>
2014-11-05 13:59:31 +0300

    Update bufferOverflow.c
    updated code

--- buffOverflow/bufferOverflow.c
+++ buffOverflow/bufferOverflow.c
@@ -9,21 +9,22 @@ void roots()
 
 int checked(char password[])
 {
-	int pass = 0;
-	char buffer[10];
-	strcpy(buffer, password);
-	if (!(strcmp(buffer, "password"))) pass = 1;
-	return pass;
+    int pass = 0;
+    char buffer[10];
+    strcpy(buffer, password);
+    if (!(strcmp(buffer, "password"))) pass = 1;
+    return pass;
 }
+
 int main(void)
 {
-	char string[50];
+    char string[50];
 
     printf("\n Enter the password : \n");
     gets(string);
 
     if (checked(string)) roots();
-	else printf("\n Wrong Password \n");
+    else printf("\n Wrong Password \n");
     
-	return 0;
+    return 0;
 }
