a5281dd9dbf5a77e0367d61eebf5798b74bcce0b
Mikhail Wall <mikhwall@gmail.com>
2014-12-17 14:17:07 +0300

    change style

--- Long Arifm/LongArifm.c
+++ Long Arifm/LongArifm.c
@@ -9,23 +9,23 @@ void longNum_input(longNum *result)
     result-> numSize = 0;
     result-> isCalculated = 0;
     char c = ' ';
-	list temp = NULL;
+    list temp = NULL;
 
     while(1) 
-	{
+    {
         scanf("%c", &c);
         if (c == '-')
-		{
-            !!(result->sign) ? (result->sign = 0) : (result->sign = -1);
+	{
+        	!!(result->sign) ? (result->sign = 0) : (result->sign = -1);
         }
         if ((c >= 48) && (c <= 57)) 
-		{
-            list_addElm(&temp, (int)(c - '0'));
-            result->numSize++;
+	{
+        	list_addElm(&temp, (int)(c - '0'));
+        	result->numSize++;
         }
         else if (c == 'q')
-		{
-            break;
+	{
+        	break;
         }
     }
     result->digits = temp;
@@ -34,41 +34,41 @@ void longNum_input(longNum *result)
 int longNum_less(longNum first, longNum second) 
 {
     if (first.numSize > second.numSize) 
-	{
+    {
         return 2;
     }
 
     else if(first.numSize < second.numSize) 
-	{
+    {
         return 1;
     }
 
     else
-	{
-		list rev1 = NULL,
-             rev2 = NULL;
+    {
+	list rev1 = NULL,
+        rev2 = NULL;
         list_reverse(&rev1, first.digits);
         list_reverse(&rev2, second.digits);
 
         while ((rev1->data == rev2->data) && (rev1->next != NULL)) 
-		{
+	{
             rev1 = rev1->next;
             rev2 = rev2->next;
         }
         if (rev1->data < rev2->data)
-		{
+	{
             list_delList(&rev1);
             list_delList(&rev2);
             return 1;
         }
         else if (rev1->data > rev2->data)
-		{
+	{
             list_delList(&rev1);
             list_delList(&rev2);
             return 2;
         }
         else
-		{
+	{
             list_delList(&rev1);
             list_delList(&rev2);
             return 0;
@@ -82,56 +82,56 @@ void longNum_sum(longNum *res, longNum first, longNum second)
     res->digits = NULL;
     int dec = 0;
 
-	if (first.sign == second.sign)
+    if (first.sign == second.sign)
+    {
+	res->sign = first.sign;
+	while ((first.digits != NULL) && (second.digits != NULL)) 
 	{
-		res->sign = first.sign;
-		while ((first.digits != NULL) && (second.digits != NULL)) 
-		{
-			list_addElm(&(res->digits), (first.digits->data + second.digits->data + dec) % 10);
-			dec = (first.digits->data + second.digits->data + dec) / 10;
-			first.digits = first.digits->next;
-			second.digits = second.digits->next;
+		list_addElm(&(res->digits), (first.digits->data + second.digits->data + dec) % 10);
+		dec = (first.digits->data + second.digits->data + dec) / 10;
+		first.digits = first.digits->next;
+		second.digits = second.digits->next;
         }
 
-		if (first.numSize > second.numSize)
+	if (first.numSize > second.numSize)
+	{
+                int i = 0;
+		for (i; i < first.numSize - second.numSize; i++) 
 		{
-            int i = 0;
-			for (i; i < first.numSize - second.numSize; i++) 
-			{
-				list_addElm(&(res->digits), (first.digits->data + dec) % 10);
-				dec = (first.digits->data + dec) / 10;
-				first.digits = first.digits->next;
-            }
+			list_addElm(&(res->digits), (first.digits->data + dec) % 10);
+			dec = (first.digits->data + dec) / 10;
+			first.digits = first.digits->next;
+                }
         }
-		else if (first.numSize < second.numSize)
+	else if (first.numSize < second.numSize)
+	{
+                int i = 0;
+		for (i; i < second.numSize - first.numSize; i++) 
 		{
-            int i = 0;
-			for (i; i < second.numSize - first.numSize; i++) 
-			{
-				list_addElm(&(res->digits), (second.digits->data + dec) % 10);
-				dec = (second.digits->data + dec) / 10;
-				second.digits = second.digits->next;
-            }
+			list_addElm(&(res->digits), (second.digits->data + dec) % 10);
+			dec = (second.digits->data + dec) / 10;
+			second.digits = second.digits->next;
+                }
         }
         if (dec == 1)
-		{
+	{
             list_addElm(&(res->digits), 1);
         }
     }
     else
+    {
+	if (longNum_less(first, second) == 1)
 	{
-		if (longNum_less(first, second) == 1)
-		{
-			longNum_subtr(res, second, first);
-			res->sign = second.sign;
+		longNum_subtr(res, second, first);
+		res->sign = second.sign;
         }
-		else if (longNum_less(first, second) == 2) 
-		{
-			longNum_subtr(res, first, second);
-			res->sign = first.sign;
+	else if (longNum_less(first, second) == 2) 
+	{
+		longNum_subtr(res, first, second);
+		res->sign = first.sign;
         }
         else
-		{
+	{
             list_addElm(&(res->digits), 0);
         }
     }
@@ -141,13 +141,13 @@ void longNum_subtr(longNum *res, longNum bigger, longNum less)
 {
     res->digits = NULL;
     while ((bigger.digits != NULL) && (less.digits != NULL))
-	{
+    {
         if (bigger.digits->data >= less.digits->data)
-		{
+	{
             list_addElm(&(res->digits), bigger.digits->data - less.digits->data);
         }
         else
-		{
+	{
             list_addElm(&(res->digits), (bigger.digits->data + 10) - less.digits->data);
             bigger.digits->next->data--;
         }
@@ -157,10 +157,10 @@ void longNum_subtr(longNum *res, longNum bigger, longNum less)
     }
 
     if (bigger.numSize > less.numSize) 
-	{
+    {
         int i = 0;
         for(i; i < bigger.numSize - less.numSize; i++) 
-		{
+	{
             list_addElm(&(res->digits), bigger.digits->data);
             bigger.digits = bigger.digits->next;
         }
@@ -169,29 +169,29 @@ void longNum_subtr(longNum *res, longNum bigger, longNum less)
 
 void longNum_showNum(longNum target)
 {
-	list rev = NULL;
+    list rev = NULL;
     if(target.isCalculated == 0) 
-	{
+    {
         if(target.sign == 0)
-		{
+	{
             list_reverse(&rev, target.digits);
             list_print(rev);
         }
         else
-		{
+	{
             list_reverse(&rev, target.digits);
             printf("-");
             list_print(rev);
         }
     }
     else
-	{
+    {
         if(target.sign == 0) 
-		{
+	{
             list_print(target.digits);
         }
         else
-		{
+	{
             printf("-");
             list_print(target.digits);
         }
