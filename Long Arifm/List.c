7bc5c6e51bd98c0ca684923f7f52f4aa9e3d2425
Mikhail Wall <mikhwall@gmail.com>
2014-12-17 14:04:53 +0300

    change style

--- Long Arifm/List.c
+++ Long Arifm/List.c
@@ -9,14 +9,14 @@ void list_addElm(list *pointer, int data)
 		*pointer = (list)malloc(sizeof(struct List));
 		(*pointer)->data = data;
 		(*pointer)->next = NULL;
-    }
-    else 
+        }
+        else 
 	{
 		list temp = (list)malloc(sizeof(struct List));
 		temp->data = data;
 		temp->next = *pointer;
 		*pointer = temp;
-    }
+        }
 }
 
 void list_delElm(list *pointer, int data) 
@@ -24,10 +24,10 @@ void list_delElm(list *pointer, int data)
 	if ((*pointer)->data == data) 
 	{
 		list temp = *pointer;
-        *pointer = (*pointer)->next;
+                *pointer = (*pointer)->next;
 		free(temp);
-    }
-    else
+        }
+        else
 	{
 		list head = *pointer;
 		while ((*pointer)->next->data != data) 
@@ -35,61 +35,62 @@ void list_delElm(list *pointer, int data)
 			(*pointer) = (*pointer)->next;
 			if ((*pointer)->next == NULL)
 			{
-                printf("There is no such number\n");
+                        	printf("There is no such number\n");
 				*pointer = head;
-                return;
-            }
-        }
+                		return;
+            		}
+        	}
 		list temp = (*pointer)->next;
 		(*pointer)->next = (*pointer)->next->next;
 		free(temp);
 		*pointer = head;
-    }
+    	}
 	if (*pointer == NULL) 
 	{
-        printf("Sorry, I cannot delete nothing :(\n");
-    }
+        	printf("Sorry, I cannot delete nothing :(\n");
+    	}
 }
 
 void list_print(list pointer)
 {
 	if (pointer == NULL) 
 	{
-        printf("There is nothing in this list\n");
-    }
-    else 
+        	printf("There is nothing in this list\n");
+    	}
+    	else 
 	{
 		while (pointer != NULL)
 		{
 			printf("%d", pointer->data);
 			pointer = pointer->next;
-        }
-        printf("\n");
-    }
+        	}
+        	printf("\n");
+    	}
 }
 
 void list_reverse(list *res, list target)
 {
     while (target != NULL) 
 	{
-        list_addElm(res, target->data);
-        target = target->next;
-    }
+        	list_addElm(res, target->data);
+        	target = target->next;
+    	}	
 }
 
 void list_delList(list *pointer) 
 {
 	if (*pointer == NULL) 
 	{
-        printf("Sorry, there is no list to delete :(\n");
-    }
-    else 
+        	printf("Sorry, there is no list to delete :(\n");
+    	}
+    	else 
 	{
 		list del = *pointer;
-		while ((*pointer) != NULL) {
+		while ((*pointer) != NULL)
+		{
 			del = *pointer;
 			*pointer = (*pointer)->next;
 			free(del);
-        }
-    } 
+        	}
+    	} 
 }
