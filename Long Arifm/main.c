603d06273e5aa6838e81110e9c9930e7fd94899b
Mikhail Wall <mikhwall@gmail.com>
2014-12-17 14:20:01 +0300

    change style

--- Long Arifm/main.c
+++ Long Arifm/main.c
@@ -6,11 +6,11 @@ int main()
 {
 	longNum a = {0};
 	longNum b = {0};
-    longNum sum= {0};
+    	longNum sum= {0};
 	longNum_input(&a);
 	longNum_input(&b);
 	longNum_sum(&sum, a, b);
 	longNum_showNum(sum);
 
-    return 0;
+    	return 0;
 }
