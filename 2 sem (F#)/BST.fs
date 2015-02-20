faabe537a4cf3d95d8b6bfb89fb1e07642b24163
Mikhail Wall <mikhwall@gmail.com>
2015-02-21 01:48:30 +0300

    BST: add type, insert, delete

---
+++ 2 sem (F#)/BST.fs
@@ -0,0 +1,49 @@
+﻿// Binary Search Tree
+// Main operations
+// Author: Mikhail Wall
+
+type Tree =
+  | Empty
+  | Node of int * Tree * Tree
+
+let rec treeInsert x tree =
+  match x, tree with 
+  | x, Empty -> Node (x, Empty, Empty)
+  | x, Node (center, left, right) ->
+    if x = center
+    then Node (x, left, right)
+    else if x < center 
+    then Node (center, treeInsert x left, right)
+    else Node (center, left, treeInsert x right)
+
+let rec findMin x = 
+  match x with 
+  | Empty -> 0
+  | Node (center, Empty, _) -> center
+  | Node (center, left, _) -> findMin left
+
+
+let rec treeRemove x tree =
+  match x, tree with
+  | x, Empty -> Empty
+  | x, Node (center, left, right) ->
+    if x > center
+    then Node (center, left, treeRemove x right) 
+    else if x < center 
+    then Node (center, treeRemove x left, right)
+    else
+      match left, right with 
+      | Empty, Empty -> Empty
+      | Empty, right -> right
+      | left, Empty -> left
+      | left, right -> 
+        if left = Empty 
+        then Node(findMin right, left, treeRemove (findMin right) right)
+        else Node(findMin left, treeRemove (findMin left) left, right)
+
+     
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
