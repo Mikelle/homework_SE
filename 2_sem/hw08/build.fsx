b70cb32e8558ac76815741f53ad8c3cff647769c
Mikhail Wall <mikhwall@gmail.com>
2015-04-29 20:37:19 +0300

    add Converter from dec, bin, hex to dec, bin, hex numbers

--- 2_sem/hw08/build.fsx
+++ 2_sem/hw08/build.fsx
@@ -17,26 +17,14 @@ Target "BuildApp" (fun _ ->
     |> Log "AppBuild-Output: "
 )
 
-Target "BuildTest" (fun _ ->
-  !! "src/test/**/*.fsproj"
-    |> MSBuildDebug testDir "Build"
-    |> Log "BuildTest-Output: "
-)
 
-Target "Test" (fun _ ->
-  !! (testDir + "/*.dll")
-    |> NUnit (fun p ->
-        {p with
-           DisableShadowCopy = true;
-           OutputFile = testDir + "TestResults.xml" })
-)
 
 
 
 "Clean"
   ==> "BuildApp"
-  ==> "BuildTest"
-  ==> "Test"
+
+
  
 
-RunTargetOrDefault "Test"
+RunTargetOrDefault "BuildApp"
--- 2_sem/hw08/src/app/Program.fs
+++ 2_sem/hw08/src/app/Program.fs
@@ -9,11 +9,15 @@ open System.Linq
 open System.Text
 open System.Windows.Forms
 open System.Globalization
+open System.Text.RegularExpressions
 
 let mutable operand1 = ""
 let mutable operand2 = ""
 let mutable operation = ' '
 let mutable res = ""
+let flagDec = ref true
+let flagHex = ref false
+let flagBin = ref false
 
 
 let programOutput =
@@ -105,25 +109,33 @@ let butEq =
     if (operand2 = "") || (operation = ' ')
     then operand1 |> ignore
     else
-      opr1 <- Double.Parse(operand1)
-      opr2 <- Double.Parse(operand2)
-      match operation with
-        | '+' ->
-          res <- (opr1 + opr2).ToString() 
-        | '-' ->
-          res <- (opr1 - opr2).ToString()
-        | '*' ->
-          res <- (opr1 * opr2).ToString()
-        | '/' ->
-          if (opr2 <> 0.0)
-          then res <- (opr1 / opr2).ToString()
-          else MessageBox.Show("Can't divide by zero!") |> ignore
-        | _-> MessageBox.Show("Unknown operator!") |> ignore
-      programOutput.Text <- res
-    operand1 <- ""
-    operand2 <- ""
-    operation <- ' '
-      )
+      if !flagBin || !flagHex 
+      then
+        MessageBox.Show("Calculator can do math only in decimal numbers.\nPress Dec button") |> ignore
+        operand1 <- ""
+        operand2 <- ""
+        operation <- ' '
+        programOutput.Text <- ""
+      else
+        opr1 <- Double.Parse(operand1)
+        opr2 <- Double.Parse(operand2)
+        match operation with
+          | '+' ->
+            res <- (opr1 + opr2).ToString() 
+          | '-' ->
+            res <- (opr1 - opr2).ToString()
+          | '*' ->
+            res <- (opr1 * opr2).ToString()
+          | '/' ->
+            if (opr2 <> 0.0)
+            then res <- (opr1 / opr2).ToString()
+            else MessageBox.Show("Can't divide by zero!") |> ignore
+          | _-> MessageBox.Show("Unknown operator!") |> ignore
+        programOutput.Text <-  res
+        operand1 <- ""
+        operand2 <- ""
+        operation <- ' '
+        )
   but
 
 let butClear =
@@ -217,34 +229,127 @@ let butDel =
   but.Size <- System.Drawing.Size(40, 30)
   but.Location <- System.Drawing.Point(160, 120)
   but.Click.Add (fun _ ->
-    programOutput.Text <- programOutput.Text.[0..programOutput.Text.Length - 2])
+    if programOutput.Text = "" then programOutput.Text <- programOutput.Text
+    else programOutput.Text <- programOutput.Text.[0..programOutput.Text.Length - 2])
   but
+
+
+
+let rec decButton  =
+  let but = new Button()
+  but.Text <- "Dec"
+  but.Location <- Point(0, 150)
+  but.Size <- Size(80, 30)
+  but.Click.Add (fun _ ->
+    if programOutput.Text <> ""
+    then
+      let output = ref 0
+      if Int32.TryParse(programOutput.Text, System.Globalization.NumberStyles.HexNumber, null, output)
+        && !flagHex
+      then 
+        programOutput.Text <- (!output).ToString()
+        flagDec := true
+        flagHex := false
+      elif Regex.IsMatch(programOutput.Text, "^[01]+$") && !flagBin
+      then 
+        programOutput.Text <- (Convert.ToInt32(programOutput.Text, 2)).ToString()
+        flagDec := true
+        flagBin := false
+      elif !flagDec
+      then programOutput.Text <- programOutput.Text
+    else 
+      flagDec := true
+      flagHex := false
+      flagBin := false)
+  but
+  
+let rec binButton =
+  let but = new Button()
+  but.Text <- "Bin"
+  but.Location <- Point(80, 150)
+  but.Size <- Size(80, 30)
+  but.Click.Add (fun _ -> 
+      
+    let output = ref 0
+    if programOutput.Text <> "" 
+    then
+      if Int32.TryParse(programOutput.Text, System.Globalization.NumberStyles.HexNumber, null, output)
+        && !flagHex         
+      then 
+        programOutput.Text <- Convert.ToString(Int32.Parse((!output).ToString()), 2)
+        flagBin := true
+        flagHex := false
+      elif Regex.IsMatch(programOutput.Text, "^[01]+$") && !flagBin
+      then programOutput.Text <- programOutput.Text
+      elif !flagDec 
+      then 
+        programOutput.Text <- Convert.ToString((Int32.Parse(programOutput.Text)), 2)
+        flagBin := true
+        flagDec := false
+    else 
+      flagBin := true
+      flagHex := false
+      flagDec := false)
+  but
+
+let rec hexButton =
+  let but = new Button()
+  but.Text <- "Hex"
+  but.Size <- Size(80, 30)
+  but.Location <- Point(160, 150)
+  but.Click.Add (fun _ ->
+    if programOutput.Text <> ""
+    then
+      let output = ref 0
+      if Regex.IsMatch(programOutput.Text, "^[01]+$") && !flagBin
+      then 
+        programOutput.Text <- Convert.ToString(Int32.Parse((Convert.ToInt32(programOutput.Text, 2)).ToString()), 16)
+        flagHex := true
+        flagBin := false
+      elif Int32.TryParse(programOutput.Text, System.Globalization.NumberStyles.HexNumber, null, output)
+        && !flagHex
+      then programOutput.Text <- programOutput.Text
+      elif !flagDec
+      then 
+        programOutput.Text <- Convert.ToString((Int32.Parse(programOutput.Text)), 16)
+        flagHex := true
+        flagDec := false
+      else  
+        flagHex := true
+        flagDec := false
+        flagBin := false)
+  but
+  
+
+
 
 let mainForm =
   let form = new Form (Visible = false, TopMost = true)
-  form.ClientSize <- Size(240, 150)
-
+  form.ClientSize <- Size(240, 180)
+  form.MaximumSize <- Size(256, 219)
+  form.MinimumSize <- Size(256, 189)
   for x in 0..9 do
     let but = new Button()
     but.Text <- x.ToString()
     but.Size <- System.Drawing.Size(40, 30)
     match x with 
-    | 0 -> but.Location <- System.Drawing.Point(40, 120)
-    | 1 -> but.Location <- System.Drawing.Point(0, 30)
-    | 2 -> but.Location <- System.Drawing.Point(40, 30)
-    | 3 -> but.Location <- System.Drawing.Point(80, 30)      
-    | 4 -> but.Location <- System.Drawing.Point(0, 60)      
-    | 5 -> but.Location <- System.Drawing.Point(40, 60)      
-    | 6 -> but.Location <- System.Drawing.Point(80, 60)      
-    | 7 -> but.Location <- System.Drawing.Point(0, 90)      
-    | 8 -> but.Location <- System.Drawing.Point(40, 90)      
-    | 9 -> but.Location <- System.Drawing.Point(80, 90)
-    | _ -> failwith ""
+      | 0 -> but.Location <- System.Drawing.Point(40, 120)
+      | 1 -> but.Location <- System.Drawing.Point(0, 30)
+      | 2 -> but.Location <- System.Drawing.Point(40, 30)      
+      | 3 -> but.Location <- System.Drawing.Point(80, 30)
+      | 4 -> but.Location <- System.Drawing.Point(0, 60)    
+      | 5 -> but.Location <- System.Drawing.Point(40, 60)
+      | 6 -> but.Location <- System.Drawing.Point(80, 60)   
+      | 7 -> but.Location <- System.Drawing.Point(0, 90)     
+      | 8 -> but.Location <- System.Drawing.Point(40, 90)     
+      | 9 -> but.Location <- System.Drawing.Point(80, 90)
+      | _ -> failwith ""
       
     but.Click.Add(fun _ -> 
       if programOutput.Text = "0"
       then programOutput.Text <- x.ToString()
       else programOutput.Text <- programOutput.Text + x.ToString())
+
     form.Controls.Add(but)
 
   form.Controls.Add(butEq)
@@ -262,6 +367,9 @@ let mainForm =
   form.Controls.Add(butPlMin)
   form.Controls.Add(programOutput)
   form.Controls.Add(butDel)
+  form.Controls.Add (decButton)
+  form.Controls.Add (binButton)
+  form.Controls.Add (hexButton)
   form
     
 
